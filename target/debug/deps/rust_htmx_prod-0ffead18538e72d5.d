b2ac7ec6dcb34fc742e296229395c465139a9226
Sam Schoenberg <119435214+sam41306061@users.noreply.github.com>
2024-06-11 19:43:14 -0600

    starting-setup

---
+++ target/debug/deps/rust_htmx_prod-0ffead18538e72d5.d
@@ -0,0 +1,5 @@
+/Users/sam4130/Documents/Code/personal/rust-htmx-prod/target/debug/deps/librust_htmx_prod-0ffead18538e72d5.rmeta: src/main.rs
+
+/Users/sam4130/Documents/Code/personal/rust-htmx-prod/target/debug/deps/rust_htmx_prod-0ffead18538e72d5.d: src/main.rs
+
+src/main.rs:
---
+++ target/debug/deps/rust_htmx_prod-8b5230dd307542f4.d
@@ -0,0 +1,5 @@
+/Users/sam4130/Documents/Code/personal/rust-htmx-prod/target/debug/deps/librust_htmx_prod-8b5230dd307542f4.rmeta: src/main.rs
+
+/Users/sam4130/Documents/Code/personal/rust-htmx-prod/target/debug/deps/rust_htmx_prod-8b5230dd307542f4.d: src/main.rs
+
+src/main.rs:
